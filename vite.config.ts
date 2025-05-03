c58e65255ed3d038066f3cc007af44bab7ca262d
diptoroy20221003010 <roydipto607@gmail.com>
2025-05-03 13:52:15 +0000

    push these changes to your GitHub repository

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
   base:
     process.env.NODE_ENV === "development"
       ? "/"
-      : process.env.VITE_BASE_PATH || "/portfolio/",
-  // Change '/portfolio/' to '/' if deploying to a custom domain
+      : process.env.VITE_BASE_PATH || "/dipto-ray-portfolio/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
